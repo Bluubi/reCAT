1b1e7b11af17422e272585beb44778f3753cb616
BluubiDev <bluubi.dev@gmail.com>
2023-11-28 20:36:42 +0100

    #5 stylized header and footer

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.css'
 export const HeaderComponent = () => {
 	return (
 		<header className={styles.header}>
-			<div>
+			<div className={styles.container}>
 				<h1>This is the header</h1>
 			</div>
 		</header>
--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App.tsx'
-import {HomePage} from "../features/home/HomePage.tsx";
+import { HomePage } from '../features/home/HomePage.tsx'
 
 export const Routes = createBrowserRouter([
 	{
@@ -10,9 +10,6 @@ export const Routes = createBrowserRouter([
 				<App />
 			</>
 		),
-		children: [
-			{ path: '/',
-			element: <HomePage />}
-		]
+		children: [{ path: '/', element: <HomePage /> }],
 	},
 ])
